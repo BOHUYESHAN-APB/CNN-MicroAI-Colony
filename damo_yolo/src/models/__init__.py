fc0fb71c84066639027d80e7ea0e530d4b8c74de
XingZhiShengLuo <65199880+BOHUYESHAN-APB@users.noreply.github.com>
2025-03-12 08:02:45 +0800

    回复

--- damo_yolo/src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DAMO-YOLO models module.
-
-This package contains the implementation of DAMO-YOLO models for colony detection.
-"""
-
-from .damo import DAMODetector, ConvBlock, DAMONeck, DAMOHead
-
-__all__ = ['DAMODetector', 'ConvBlock', 'DAMONeck', 'DAMOHead']
